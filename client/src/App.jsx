4b846f0ee3c344715fadd7169612e0d69a9b856a
UdayGorai <udaygorai111@gmail.com>
2024-06-03 16:05:18 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { Navbar,Welcome,Footer,Services,Transactions } from "./components" ;
 
  
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen">
         <div className="gradient-bg-welcome">
